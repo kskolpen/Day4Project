76234218944a9fc278584e8382f19ab305a3011a
Kirill Skolpen <kskolpen@gmail.com>
2021-05-25 16:24:40 -0700

    shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -3,5 +3,11 @@ public class BranchingPractice {
     public static void main(String[] args) {
 
         System.out.println("Hello");
+
+        //created new project
+        //enabled version control vsc
+        //added .gitignore with .idea , target folder and .iml file
+        //made initial commit by keeping track of all files
+        //shared project on github
     }
 }
